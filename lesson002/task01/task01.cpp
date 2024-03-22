cc4cfebd7ea30ead6e86324930bbbf025b92f683
Andrei Beliaev <ab5030107021@yandex.ru>
2024-03-23 01:31:03 +0300

    lesson 02, task 01 updated

--- lesson002/task01/task01.cpp
+++ lesson002/task01/task01.cpp
@@ -15,29 +15,76 @@ std::string path = "/large/data2/Home/Andrew/Documents/geekbrains/CPP_advanced/l
 
 int main()
 {
-    void wrteFile(char[]);
+    void writeFile(std::string);
     bool validateDate(std::string);
-    bool validatePayment(std::string);
+    bool validateNumbers(std::string, bool isDate = false);
+    bool validateName(std::string);
     std::string record;
 
-    std::string tmp = "";
+    bool flag = true;
 
-    while (tmp != "-2")
+    while (flag)
     {
+        std::string tmp = "";
+        int complete = 0;
+
+        // if (tmp == "exit" || tmp == "Exit" || tmp == "EXIT")
+        //     break;
+
+        std::cout << "Input Name: ";
+        std::cin >> tmp;
+        std::cout << "validate amount: " << validateName(tmp) << std::endl;
+        if (validateName(tmp))
+        {
+            record += tmp + " ";
+            complete++;
+        }
+        std::cout << "Input Surname: ";
+        std::cin >> tmp;
+        std::cout << "validate amount: " << validateName(tmp) << std::endl;
+        if (validateName(tmp))
+        {
+            record += tmp + " ";
+            complete++;
+        }
 
         std::cout << "Input date (YYYY.MM.DD): ";
         std::cin >> tmp;
-        if (tmp != "-2")
-            std::cout << "validate date: " << validateDate(tmp) << std::endl;
+        std::cout << "validate date: " << validateDate(tmp) << std::endl;
+        if (validateDate(tmp))
+        {
+            record += tmp + " ";
+            complete++;
+        }
+
+        std::cout << "Input amount: ";
+        std::cin >> tmp;
+        std::cout << "validate amount: " << validateNumbers(tmp) << std::endl;
+        if (validateNumbers(tmp))
+        {
+            record += tmp + "\n";
+            complete++;
+        }
+
+        if (complete == 5)
+            writeFile(record);
+
+        std::cout << "Proceed to next record (Y/n)";
+        std::cin >> tmp;
+        if (tmp == "n" || tmp == "N")
+            flag = false;
     }
+
     return 0;
 }
 
-void writeFile(char record[])
+void writeFile(std::string record)
 {
     std::ofstream file;
     file.open(path, std::ios::binary);
-    file.write(record, sizeof(*record));
+    std::cout << "Recording: " << record << std::endl;
+    // file.write(record, sizeof(*record));
+    file.close();
 }
 
 bool validateDate(std::string date)
@@ -45,10 +92,8 @@ bool validateDate(std::string date)
     bool validateNumbers(std::string, bool);
 
     if (!validateNumbers(date, true))
-    {
-        std::cout << "1st" << std::endl;
         return false;
-    }
+
     bool leapYear(int);
 
     int months31[] = {1, 3, 5, 7, 8, 10, 12};
@@ -58,76 +103,86 @@ bool validateDate(std::string date)
     int day = std::stoi(date.substr(8, 9));
 
     if (day < 1 || year < 1970 || month < 1 || month > 12)
-    {
-        std::cout << "2nd" << std::endl;
         return false;
-    }
 
     if (month == 2)
     {
         if (leapYear(year) && day <= 29)
-        {
-            std::cout << "3rd" << std::endl;
             return true;
-        }
         else
-        {
-            std::cout << "4th" << std::endl;
             return day <= 28;
-        }
     }
     else
     {
         for (int i = 0; i < sizeof(months31); i++)
-        {
             if (months31[i] == month)
-            {
-                std::cout << "5th" << std::endl;
                 return day <= 31;
-            }
-        }
     }
-    std::cout << "5th" << std::endl;
     return day <= 30;
 }
 
 bool validateNumbers(std::string numLine, bool isDate = false)
 {
+    bool flag = false;
     std::cout << "isDate: " << isDate << std::endl;
     int countDots = 0;
-    /* codes for 0, 1, 2, 3, 4, 6, 8, 9*/
+    /* below: ASCII codes for 0, 1, 2, 3, 4, 6, 8, 9 */
     int nums[] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
 
     for (int i = 0; i < numLine.size(); i++)
     {
-        std::cout << "num validator " << numLine[i] << std::endl;
-        for (int i = 0; i < sizeof(nums); i++)
+        if (numLine[i] == '.')
         {
-            if (numLine[i] == '.')
-            {
-                countDots++;
-                if (isDate && countDots > 2 || !isDate && countDots > 1)
-                    return false;
-            }
-            if (nums[i] == (int)numLine[i] || numLine[i] == '.')
-                continue;
-            else
-            {
-                // todo RMS
-                std::cout << "\n"
-                          << "nums: "
-                          << nums[i]
-                          << " (int)numLine[i]: "
-                          << (int)numLine[i]
-                          << std::endl;
+            countDots++;
+            if (isDate && countDots > 2 || !isDate && countDots > 1)
                 return false;
+        }
+        else
+        {
+            // std::cout << "num validator " << numLine[i] << std::endl;
+            for (int j = 0; j < sizeof(nums); j++)
+            {
+                // if (nums[j] == (int)numLine[i])
+                if ((int)numLine[i] >= 48 && (int)numLine[i] <= 57)
+                    flag = true;
+                else
+                {
+                    // todo RMS
+                    std::cout << "\n"
+                              << "nums: "
+                              << nums[i]
+                              << " (int)numLine[i]: "
+                              << (int)numLine[i]
+                              << std::endl;
+                }
             }
         }
     }
-    return true;
+    return flag;
 }
 
 bool leapYear(int year)
 {
     return year % 4 == 0 || year % 400 == 0 && year % 100 != 0;
+}
+
+bool validateName(std::string name)
+{
+    int upperLettersA = 65;
+    int upperLettersZ = 90;
+
+    bool validateNumbers(std::string);
+
+    if (validateNumbers)
+    {
+        std::cout << "Name/surname should not contain numbers."
+                  << std::endl;
+        return false;
+    }
+    if ((int)name[0] < upperLettersA || (int)name[0] > 90)
+    {
+        std::cout << "Filrst letter must be capital."
+                  << std::endl;
+        return false;
+    }
 }
