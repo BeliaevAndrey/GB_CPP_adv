8d52baa5b7f4f40b2830d86d1388c6f57ca75513
Andrei Beliaev <ab5030107021@yandex.ru>
2024-03-27 23:13:47 +0300

    lesson 04, task 01 updated

--- lesson004/task01/task01.cpp
+++ lesson004/task01/task01.cpp
@@ -19,14 +19,18 @@ Ivanov — узнать телефон абонента по фамилии
 #include <string>
 #include <map>
 
-std::string CONDITIONS = "Request format:"
+std::string CONDITIONS = "Формат запроса:\n"
                          "XX-XX-XX Surname -- добавить телефон и фамилию абонента в справочник\n"
-                         "where 'X' -- is strictly a digit ('-' sign is also allowed)\n"
                          "XX-XX-XX -- узнать фамилию абонента по номеру телефона\n"
-                         "Surname -- узнать телефон абонента по фамилии\n";
+                         "Surname -- узнать телефон абонента по фамилии\n"
+                         "где: 'X' -- строго цифра (допустим разделитель '-'\n"
+                         "фамилия -- строго латиницей\n"
+                         "\nprint book -- вывести весь сравочник\n"
+                         "exit -- выход\n";
 
 std::map<std::string, std::string> pBook;
 
+/* Вывести на экран всю книгу */
 void printPhoneBook()
 {
     std::cout << "\nPhone book content: " << std::endl;
@@ -38,18 +42,31 @@ void printPhoneBook()
     std::cout << std::endl;
 }
 
-int getPhone(std::pair<std::string, std::vector<std::string>> answer)
+/* получить список номеров по фамилии */
+int getPhone(std::string &surname,
+             std::vector<std::pair<std::string, std::string>> &answer)
 {
-    return 2;
+    for (std::map<std::string, std::string>::iterator it = pBook.begin();
+         it != pBook.end(); it++)
+    {
+        if (it->second == surname)
+        {
+            std::pair<int, int> newPair();
+            answer.push_back(
+                std::pair<std::string, std::string>(it->second, it->first));
+        }
+    }
+    return answer.size() > 0 ? 2 : -2;
 }
 
+/* Получить фамилию но номеру */
 int getSurname(std::string &phone,
                std::pair<std::string, std::string> &answer)
 {
     for (std::map<std::string, std::string>::iterator it = pBook.begin();
          it != pBook.end(); it++)
     {
-        if (it->second == phone)
+        if (it->first == phone)
         {
             std::pair<int, int> newPair();
             answer = std::pair<std::string, std::string>(it->first, it->second);
@@ -59,125 +76,116 @@ int getSurname(std::string &phone,
     return -2;
 }
 
+/* Валидация цифр */
 bool validateLetter(char letter)
 {
     int cLower = 65, cUpper = 90,
         nLower = 90, nUpper = 122;
     int code = (int)letter;
-    std::cout << "letter: " << letter << std::endl;
     return (code >= cLower && code <= cUpper) ||
            (code >= nLower && code <= nUpper);
 }
 
-bool validateDigit(char digit)
+/* Валидация букв*/
+bool validateDigit(char &digit)
 {
     int lower = 48, upper = 57;
-    // todo SEMAPHORE
-    std::cout << "digit: " << digit << " " << (int)digit << std::endl
-              << (((int)digit >= lower && (int)digit <= upper) || (digit = '-'))
-              << std::endl;
     return (((int)digit >= lower && (int)digit <= upper) ||
-            (digit = '-'));
+            (digit == '-'));
 }
 
+/* Распарсить запрос и вызвать необходимую фукнцию */
 int parseReqAndWrite(std::string &request,
                      std::pair<std::string, std::string> &answer,
-                     std::pair<std::string,
-                               std::vector<std::string>> &multiAnswer)
+                     std::vector<std::pair<std::string,
+                                           std::string>> &multiAnswer)
 {
-    std::string phone = "", surname = "";
+    std::string phone = "", surname = ""; // дефолтные значения вместо флагов
     int i = 0;
     if (validateDigit(request[i]))
     {
-        std::cout << "digit on start" << std::endl; // todo SEMAPHORE
         while (request[i] != ' ' && request[i] != '\0')
         {
             if (validateDigit(request[i]))
             {
-                phone += request[i];
+                phone += request[i]; // конкатенация номера
                 ++i;
             }
             else
                 break;
         }
-
-        i += 1;
+        i += 1; // пропустить пробел
     }
-    std::cout << "i" << i << std::endl;
+
     for (; i < request.length(); i++)
         if (validateLetter(request[i]))
-        {
-            surname += request[i];
-        }
+            surname += request[i]; // конкатенация фамилии
         else
             return -1;
-
-    std::cout << "surname:      " << surname << "\n" // todo SEMAPHORE
-              << std::boolalpha << "(surname == \"\")"
-              << (surname == "") << "\n"
-              << "phone:        " << phone << "\n"
-              << "request:      " << request
-              << std::endl;
-
     if (surname == "")
-    {
+        // получить фамилию по номеру и код возврата
         return getSurname(request, answer);
-    }
-
     else if (phone == "")
-    {
-        return 2;
-    }
+        // получить список номеров по фамилии и код возврата
+        return getPhone(request, multiAnswer);
     else
-    {
+        // добавить запись в справочник
         pBook.insert(std::pair<std::string, std::string>(phone, surname));
-        return 3;
-    }
+    return 3;
 }
 
 int main()
 {
-    // std::string conditions = "Request format:"
-    //                          "XX-XX-XX Surname -- добавить телефон и фамилию абонента в справочник\n"
-    //                          "where 'X' -- is strictly a digit ('-' sign is also allowed)\n"
-    //                          "XX-XX-XX -- узнать фамилию абонента по номеру телефона\n"
-    //                          "Surname -- узнать телефон абонента по фамилии\n";
-
-    std::map<std::string, std::string> pBook;
     std::pair<std::string, std::string> answer;
-    std::pair<std::string, std::vector<std::string>> multiAnswer;
+    std::vector<std::pair<std::string, std::string>> multiAnswer;
     std::string request;
     std::cout << CONDITIONS << std::endl;
-    std::cout << "Enter your request: ";
-    std::getline(std::cin, request);
-    int aCode = parseReqAndWrite(request, answer, multiAnswer);
-    std::cout << "aCode" << aCode << std::endl;
-    switch (aCode)
+    while (true)
     {
-    case 4:
-        std::cout << "Wrong request format." << std::endl;
-        break;
-    case -2:
-        std::cout << "Not found." << std::endl;
-        break;
-    case 1:
-        std::cout << "Found: "
-                  << answer.first << " "
-                  << answer.second
-                  << std::endl;
-        break;
-    case 2:
-        std::cout << "case 2 answer->" << std::endl;
-        break;
-    case 3:
-        std::cout << "Entity is added to phone book." << std::endl;
-        break;
-    default:
-        std::cout << "Unidentified error. " << aCode << std::endl;
-        break;
+        std::cout << "Enter your request ('exit' to exit): ";
+        std::getline(std::cin, request);
+        if (request == "exit")
+            break;
+        if (request == "print book")
+            printPhoneBook();
+        else
+        {
+            int aCode = parseReqAndWrite(request, answer, multiAnswer);
+            switch (aCode)
+            {
+            case 4:
+                std::cout << "Wrong request format." << std::endl;
+                break;
+            case -2:
+                std::cout << "Not found." << std::endl;
+                break;
+            case 1:
+                std::cout << "Found: "
+                          << answer.first << " "
+                          << answer.second
+                          << std::endl;
+                break;
+            case 2:
+                std::cout << "Found: "
+                          << multiAnswer.size()
+                          << " entities"
+                          << std::endl;
+                std::cout << multiAnswer[0].first << std::endl;
+                for (int i = 0; i < multiAnswer.size(); i++)
+                {
+                    std::cout << i + 1 << " " << multiAnswer[i].second << std::endl;
+                }
+                multiAnswer.clear();
+                break;
+            case 3:
+                std::cout << "Entity is added to phone book." << std::endl;
+                break;
+            default:
+                std::cout << "Unidentified error. " << aCode << std::endl;
+                break;
+            }
+        }
     }
 
-    // std::cout << request << std::endl;
-    printPhoneBook();
     return 0;
 }
