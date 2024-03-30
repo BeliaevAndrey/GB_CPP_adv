1f7a02c33d335b701ea401f850970dda23130d01
Andrei Beliaev <ab5030107021@yandex.ru>
2024-03-30 21:55:20 +0300

    lesson 06, task 02, updated

--- lesson006/task02/task02.cpp
+++ lesson006/task02/task02.cpp
@@ -10,12 +10,83 @@
 
 #include <iostream>
 #include <string>
-#include <ctime>
 #include <map>
 
+#include <ctime>
+#include <iomanip>
+
+
+using namespace std;
+
+void insertBD(map<time_t, string>store, string& name, string& bDate)
+{
+    std::cout << "bDate: " << bDate << std::endl;
+    std::cout << "name: " << name << std::endl;
+    std::cout << "SM 1: " << std::endl;
+
+    bDate += " 00.00.00";
+    std::cout << "SM 2: " << std::endl;
+    const char* dt = bDate.c_str();
+    std::cout << "SM 3: " << std::endl;
+    std::tm* d;
+    std::cout << "SM 4: " << std::endl;
+
+    d->tm_hour = 0;
+    std::cout << "SM 4.4: " << std::endl;
+
+    d->tm_min = 0;
+    std::cout << "SM 4.5: " << std::endl;
+
+    d->tm_sec = 0;
+    std::cout << "SM 5: " << std::endl;
+
+
+    d->tm_year = std::stoi(bDate.substr(0, 4));
+    std::cout << "SM 4.1: " << std::endl;
+
+    d->tm_mon = std::stoi(bDate.substr(5, 7)) - 1;
+    std::cout << "SM 4.2: " << std::endl;
+
+    d->tm_mday = std::stoi(bDate.substr(8, 9));
+    std::cout << "SM 4.3: " << std::endl;
+
+
+    std::cout << "tm_year: " << d->tm_year << std::endl;
+    std::cout << "tm_mon:  " << d->tm_mon << std::endl;
+    std::cout << "tm_mday: " << d->tm_mday << std::endl;
+    std::cout << "tm_hour: " << d->tm_hour << std::endl;
+    std::cout << "tm_min:  " << d->tm_min << std::endl;
+    std::cout << "tm_sec:  " << d->tm_sec << std::endl;
+
+    strptime(dt, "%Y.%m.%d %H:%M:%S", d);
+    std::cout << "SM 6: " << std::endl;
+
+    std::cout << "dt: " << dt << std::endl;
+    std::cout << "SM 7: \n"
+        << "mktime(d): "
+        << mktime(d)
+        << std::endl;
+
+    pair<time_t, string> bd = make_pair(mktime(d), name);
+    store.insert(bd);
+    // store[dt] = name;
+    std::cout << "SM 8: " << std::endl;
+}
 
 int main()
 {
+    string command;
+    map<time_t, string> birthdays;
+
+    while (command != "end") {
+
+        string name, bd;
+        cout << "Input name and birthday [name YYYY.MM.DD] (\"end\" to stop): ";
+        cin >> name >> bd;
+        std::cout << "SM 0: " << std::endl;
+        insertBD(birthdays, name, bd);
+
+    }
 
     return 0;
 }
