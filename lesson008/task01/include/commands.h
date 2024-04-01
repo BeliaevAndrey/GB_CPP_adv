c0edf932bb9c7b3bd2637c2f756cdacf3fca7bb7
Andrei Beliaev <ab5030107021@yandex.ru>
2024-04-01 19:37:21 +0300

    lesson 08, task01 updated

--- lesson008/task01/include/commands.h
+++
@@ -1,5 +0,0 @@
-#define play    1
-#define pause   2
-#define next    3
-#define stop    4
-#define exit    5
--- lesson008/task01/include/player.h
+++ lesson008/task01/include/player.h
@@ -4,16 +4,15 @@
 #include "track.h"
 #include "album.h"
 
-class Player {
-public:
-    Player();
+class Player;
 
-    void play(int);                 // 1
-    void pause();                   // 2
-    void next();                    // 3
-    void stop();                    // 4
-    void exit(bool&);               // 5
-    void printPlaylist();           // 6
-    void loadPlaylist();            // 7
+// Player();
 
-};
+// void play(int);                 // 1
+// void pause();                   // 2
+// void next();                    // 3
+// void stop();                    // 4
+// void exit(bool&);               // 5
+// void printPlaylist();           // 6
+// void loadPlaylist();            // 7
+
