80870e2fdc6b631bc2f84f742b38138d2233e448
Andrei Beliaev <ab5030107021@yandex.ru>
2024-04-01 19:44:22 +0300

    lesson 08, task01 updated

--- lesson008/task01/include/player.h
+++ lesson008/task01/include/player.h
@@ -4,15 +4,24 @@
 #include "track.h"
 #include "album.h"
 
-class Player;
+class Player {
 
-// Player();
+    std::string menu;
+    Track* playlist;
+    int trackCount;
+    int nowPlay;
+    bool pause;
+    bool checkAlbumLoaded();
 
-// void play(int);                 // 1
-// void pause();                   // 2
-// void next();                    // 3
-// void stop();                    // 4
-// void exit(bool&);               // 5
-// void printPlaylist();           // 6
-// void loadPlaylist();            // 7
+public:
+    Player();
 
+    void play(int);                 // 1
+    void pause();                   // 2
+    void next();                    // 3
+    void stop();                    // 4
+    void exit(bool&);               // 5
+    void printPlaylist();           // 6
+    void loadPlaylist();            // 7
+
+};
--- lesson008/task01/src/player.cpp
+++ lesson008/task01/src/player.cpp
@@ -1,99 +1,105 @@
 #include "player.h"
 
-class Player
-{
-    std::string menu = "1. play\n"
-        "2. pause\n"
-        "3. next\n"
-        "4. stop\n"
-        "5. exit\n"
-        "6. load playlist\n"
-        "7. print playlist";
+std::string menu = "1. play\n"
+"2. pause\n"
+"3. next\n"
+"4. stop\n"
+"5. exit\n"
+"6. load playlist\n"
+"7. print playlist";
 
 
-    Track* playlist;
-    int trackCount = 0;
-    int nowPlay = -1;
-    bool pause = false;
+Track* playlist;
+int trackCount = 0;
+int nowPlay = -1;
+bool isPaused = false;
 
 
-    bool checkAlbumLoaded() {
-        if (!trackCount) {
-            std::cout << "First, load an album." << std::endl;
-            return false;
-        }
-        return true;
+bool checkAlbumLoaded() {
+    if (!trackCount) {
+        std::cout << "First, load an album." << std::endl;
+        return false;
     }
+    return true;
+}
 
-public:
-    Player() {
-        std::cout << "Player is active." << std::endl;
-        std::cout << menu << std::endl;
+Player::Player() {
+    std::cout << "Player is active." << std::endl;
+    std::cout << menu << std::endl;
+}
+void play(int num) {
+    if (isPaused) {
+        isPaused = false;
+        std::cout << "Continue playing" << std::endl;
+        playlist[nowPlay].printTrack();
     }
-    void play(int num) {
-        if (pause) pause = false;
-        if (!checkAlbumLoaded()) return;
-        if (num < trackCount) {
-            std::cout << "Playing:\n"
-                << std::endl;
-            playlist[num].printTrack();
-            nowPlay = num;
-        }
-        else std::cout << "Wrong track number" << std::endl;
+    if (!checkAlbumLoaded()) return;
+    if (num < trackCount) {
+        std::cout << "Playing:\n"
+            << std::endl;
+        playlist[num].printTrack();
+        nowPlay = num;
     }
+    else std::cout << "Wrong track number" << std::endl;
+}
 
-    void pause() {
-        if (pause) pause = false;
-        if (!checkAlbumLoaded()) return;
-        if (trackCount && nowPlay > -1) {
-            pause = true;
-        }
-        else play(0);
+void pause() {
+    if (isPaused) {
+        isPaused = false;
+        std::cout << "Continue playing" << std::endl;
+        playlist[nowPlay].printTrack();
     }
+    if (!checkAlbumLoaded()) return;
+    if (trackCount && nowPlay > -1) {
+        isPaused = true;
+    }
+    else play(0);
+}
 
-    void next() {
-        if (!checkAlbumLoaded()) return;
-        if (nowPlay > -1) {
-            play(nowPlay + 1);
-        }
-        else play(0);
+void next() {
+    if (!checkAlbumLoaded()) return;
+    if (nowPlay > -1) {
+        play(nowPlay + 1);
     }
+    else play(0);
+}
 
-    void stop() {
-        if (!checkAlbumLoaded()) return;
-        if (pause) pause = false;
-        if (nowPlay > -1) {
-            nowPlay = -1;
-            std::cout << "Stopped." << std::endl;
-            playlist[nowPlay].printTrack();
-        }
-        else return;
-    }
-    void exit(bool& activeFlag) {
-        if (checkAlbumLoaded()) {
-            delete playlist;
-            playlist = nullptr;
-        }
-        activeFlag = false;
-    }
+void stop() {
+    if (!checkAlbumLoaded()) return;
+    if (isPaused) isPaused = false;
 
-    void printPlaylist() {
-        for (int i = 0; i < trackCount; i++)
-        {
-            std::cout << i << ". "
-                << playlist[i].getSinger() << " "
-                << playlist[i].getTitle() << " "
-                << playlist[i].getDuration() << " "
-                << playlist[i].getAlbum() << " "
-                << std::endl;
-        }
+    if (nowPlay > -1) {
+        nowPlay = -1;
+        std::cout << "Stopped." << std::endl;
+        playlist[nowPlay].printTrack();
     }
+    else return;
+}
+void exit(bool& activeFlag) {
+    if (checkAlbumLoaded()) {
+        delete playlist;
+        playlist = nullptr;
+    }
+    activeFlag = false;
+}
 
-    void loadPlaylist() {
-        if (checkAlbumLoaded()) {
-            delete playlist;
-            playlist = nullptr;
-        }
-        playlist = loadAlbum();
+void printPlaylist() {
+    for (int i = 0; i < trackCount; i++)
+    {
+        std::cout << i << ". "
+            << playlist[i].getSinger() << " "
+            << playlist[i].getTitle() << " "
+            << playlist[i].getDuration() << " "
+            << playlist[i].getAlbum() << " "
+            << std::endl;
     }
-};
+}
+
+void loadPlaylist() {
+    if (checkAlbumLoaded()) {
+        delete playlist;
+        playlist = nullptr;
+    }
+    playlist = loadAlbum();
+}
+
