023438d9ae68621569cda8ee55b4a199cac168db
Andrei Beliaev <ab5030107021@yandex.ru>
2024-04-04 14:28:55 +0300

    lesson 10, task03 added

--- lesson010/task03/task03.cpp
+++ lesson010/task03/task03.cpp
@@ -27,38 +27,49 @@
 
 const int KITCHEN_LO_LIM = 5;
 const int KITCHEN_UP_LIM = 15;
+const int ORDER_LO_LIM = 5;
+const int ORDER_UP_LIM = 10;
+const int COURIER_WAIT = 30;
+const int ORDERS_AIM = 11;
 std::mutex orderLock;
 std::mutex kitchenLock;
 
-std::vector<std::string> dishes{ "Pizza", "Soup", "Steak", "Salad", "Sushi" };
+std::vector<std::string> menu{ "Pizza", "Soup", "Steak", "Salad", "Sushi" };
+std::vector<std::string> ordersWait;
 std::vector<std::string> readyDishes;
-int ordersFulfilled = 0;
+int ordersComplete = 0;
 
-void kitchen(std::string& dish) {
-    kitchenLock.lock();
+void kitchen() {
     int waitTime = (KITCHEN_LO_LIM + rand()) % KITCHEN_UP_LIM;
+    kitchenLock.lock();
+    std::string dish = ordersWait.front();
+    std::cout << "kitchen got order: " << dish << std::endl;
+    ordersWait.erase(ordersWait.begin());
     std::this_thread::sleep_for(std::chrono::seconds(waitTime));
     readyDishes.push_back(dish);
     kitchenLock.unlock();
-    std::cout << "Ready: " << dish << std::endl;
+    std::cout << "Ready: " << dish
+        << "Oders complete: "
+        << ordersComplete << std::endl;
 }
 
 void order() {
-    int dishIndex = std::rand() % dishes.size();
+    int orderDelay = (ORDER_UP_LIM + rand()) % ORDER_UP_LIM;
+    std::this_thread::sleep_for(std::chrono::seconds(orderDelay));
+    int menuIndex = std::rand() % menu.size();
     orderLock.lock();
-    std::string dish = dishes[dishIndex];
+    ordersWait.push_back(menu[menuIndex]);
     orderLock.unlock();
-    kitchen(dish);
 }
 
 void courier() {
-    std::this_thread::sleep_for(std::chrono::seconds(30));
+    std::this_thread::sleep_for(std::chrono::seconds(COURIER_WAIT));
     if (!readyDishes.empty()) {
         orderLock.lock();
         std::string dish = readyDishes.front();
         readyDishes.erase(readyDishes.begin());
-        ordersFulfilled++;
-        std::cout << dish << " taken" << std::endl;
+        ordersComplete++;
+        std::cout << dish << " taken." << std::endl;
     }
     orderLock.unlock();
 }
@@ -67,16 +78,21 @@ int main()
 {
     std::srand(std::time(nullptr));
 
-    while (ordersFulfilled < 10) {
-
+    while (ordersComplete < ORDERS_AIM) {
         std::thread threadOrder(order);
         std::thread threadCourier(courier);
 
-        threadOrder.detach();
-        threadCourier.detach();
+        threadCourier.join();
 
+        threadOrder.detach();
         if (threadOrder.joinable()) threadOrder.join();
-        if (threadCourier.joinable()) threadCourier.join();
+
+        if (!ordersWait.empty())
+        {
+            std::thread threadKitchen(kitchen);
+            threadKitchen.detach();
+            if (threadKitchen.joinable()) threadKitchen.join();
+        }
     }
 
     std::cout << "All done" << std::endl;
